c888fb94c288106f23f51d297c20f5c27c1dcbac
joren97x <babamyloves2002@gmail.com>
2023-11-24 20:13:20 +0630

    uhh

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: '/cpu-scheduling/',
+  publicPath: '/CPU-scheduling/',
   transpileDependencies: true,
 
   pluginOptions: {
